54d4387fd0530ab269f4eb0acc1977bc9e31e6de
Liz GingerKiwi.Dev <liz@gingerkiwi.dev>
2023-05-01 14:54:56 -0400

    add getCatHtml() to Cat.js

--- cat.js
+++ cat.js
@@ -4,6 +4,16 @@ class Cat {
     constructor(data) {
         Object.assign(this, data)
     }
+
+    getCatHtml() {
+        const { name, avatar, alt, age, bio, hasBeenSwiped, hasBeenLiked } = this
+        return `
+        <div class="profile">
+          <img src="${avatar}" alt="${alt}" />
+          <h2 class="cat-name" id="cat-name">${name} ${age}</h2>
+          <div class="cat-bio" id="cat-bio">${bio}</div>
+        </div>`
+    }
 }
 
 export { Cat }
