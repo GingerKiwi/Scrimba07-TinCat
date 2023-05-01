ea80c857b951924578958546ac9e730bdce43c01
Liz GingerKiwi.Dev <liz@gingerkiwi.dev>
2023-05-01 14:33:05 -0400

    add all 7 cats to data.js. add alt text placeholder to all obj in array in data.js

--- data.js
+++ data.js
@@ -1,7 +1,8 @@
-const dogs = [
+const cats = [
     {
         name: "Rex",
         avatar: "/assets/dog-rex.jpg",
+        alt: "Alt image text here!",
         age: 25,
         bio: "Art. Literature. Natural wine. Yoga.",
         hasBeenSwiped: false,
@@ -9,6 +10,7 @@ const dogs = [
     },{
         name: "Bella",
         avatar: "/assets/dog-bella.jpg",
+        alt: "Alt image text here!",
         age: 43,
         bio: "Yup, that's my owner. U can meet him if you want",
         hasBeenSwiped: false,
@@ -17,8 +19,73 @@ const dogs = [
     {
         name: "Teddy",
         avatar: "/assets/dog-teddy.jpg",
+        alt: "Alt image text here!",
         age: 30,
         bio: "How you doin?",
+        hasBeenSwiped: false,
+        hasBeenLiked: false
+    },
+    {
+        name: "Kitten",
+        avatar: "/assets/cat-kitten.jpg",
+        alt: "Alt image text here!",
+        age: 16,
+        bio: "Food, naps, cuddles, and brushing",
+        hasBeenSwiped: false,
+        hasBeenLiked: false
+    },
+    {
+        name: "Azriel",
+        avatar: "/assets/cat-azzy.jpg",
+        alt: "Alt image text here!",
+        age: 8,
+        bio: "I loves giving ginger headbutts, and catching the red dot",
+        hasBeenSwiped: false,
+        hasBeenLiked: false
+    },
+    {
+        name: "Flora",
+        avatar: "/assets/cat-flora.jpg",
+        alt: "Alt image text here!",
+        age: 16,
+        bio: "I'm a princess",
+        hasBeenSwiped: false,
+        hasBeenLiked: false
+    }
+    ,
+    {
+        name: "Boo",
+        avatar: "/assets/cat-boo.jpg",
+        alt: "Alt image text here!",
+        age: 10,
+        bio: "I'm full of love and fun and mischief!",
+        hasBeenSwiped: false,
+        hasBeenLiked: false
+    },
+    {
+        name: "Frodo",
+        avatar: "/assets/cat-frodo.jpg",
+        alt: "Alt image text here!",
+        age: 01,
+        bio: "Blueberry muffins are my jam",
+        hasBeenSwiped: false,
+        hasBeenLiked: false
+    },
+    {
+        name: "Finnigan",
+        avatar: "/assets/cat-fin.jpg",
+        alt: "Alt image text here!",
+        age: 01,
+        bio: "I make roadrunner sounds when I run",
+        hasBeenSwiped: false,
+        hasBeenLiked: false
+    },
+    {
+        name: "Maybel Victoria Anne Elizabeth",
+        avatar: "/assets/cat-frodo.jpg",
+        alt: "Alt image text here!",
+        age: 01,
+        bio: "I'm cute and snuggly - but never forget I was born on the streets!",
         hasBeenSwiped: false,
         hasBeenLiked: false
     }
